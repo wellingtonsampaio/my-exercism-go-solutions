5229e3f73d62b0c8179be73bba3be5671730038a
Wellington <wellingtonsampaio@gmail.com>
2017-04-18 19:00:16 -0300

    Fetch exercise Gigasecond

---
+++ gigasecond/gigasecond.go
@@ -0,0 +1,10 @@
+// Package clause.
+package gigasecond
+
+// Constant declaration.
+const testVersion = ? // find the value in gigasecond_test.go
+
+// API function.  It uses a type from the Go standard library.
+func AddGigasecond(time.Time) time.Time
+
+// Reviewers don't think much of stub comments.  Replace or remove.
