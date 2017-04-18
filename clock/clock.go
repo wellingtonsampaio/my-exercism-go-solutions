de8c2103f82206c0663e9c28c2801d3ab35e4236
Wellington <wellingtonsampaio@gmail.com>
2017-04-18 18:57:48 -0300

    Solution for exercise Clock

--- clock/clock.go
+++ clock/clock.go
@@ -1,26 +1,67 @@
-// Clock stub file
-
-// To use the right term, this is the package *clause*.
-// You can document general stuff about the package here if you like.
+// Package clock contains the implementation of the Exercism's go exercise 'clock'.
 package clock
+
+import (
+	"fmt"
+	"strconv"
+)
 
 // The value of testVersion here must match `targetTestVersion` in the file
 // clock_test.go.
 const testVersion = 4
 
-// Clock API as stub definitions.  No, it doesn't compile yet.
-// More details and hints are in clock_test.go.
+// The number of minutes of a day
+const dayInMinutes = 60 * 24
 
-type Clock // Complete the type definition.  Pick a suitable data type.
+// Clock represents a clock where minutes can be added or substracted.
+type Clock struct {
+	minute int
+}
 
+// New creates a new value of type Clock.
 func New(hour, minute int) Clock {
+	// create a clock with 0 minutes and then add what was passed as arguments
+	return Clock{0}.Add((hour * 60) + minute)
 }
 
-func (Clock) String() string {
+func (c Clock) String() string {
+	// calculate the hours
+	hour := c.minute / 60
+	// process it until it is not greater than 23
+	for hour > 23 {
+		hour -= 24
+	}
+
+	// process the minutes
+	minute := c.minute % 60
+
+	// return the formatted time
+	return fmt.Sprintf("%v:%v", addLeadingZero(hour), addLeadingZero(minute))
 }
 
-func (Clock) Add(minutes int) Clock {
+// Add manipulates the current time of the clock accepting positive and negative minutes.
+func (c Clock) Add(minutes int) Clock {
+	// sum the minutes
+	c.minute += minutes
+
+	// Convert negative time in the equivalent positive time
+	for c.minute < 0 {
+		c.minute += dayInMinutes
+	}
+
+	// ignore additional days
+	for c.minute > dayInMinutes {
+		c.minute -= dayInMinutes
+	}
+
+	return c
 }
 
-// Remember to delete all of the stub comments.
-// They are just noise, and reviewers will complain.
+// addLeadingZero converts an int into string adding a leading 0 if the value is lesser than 10
+func addLeadingZero(value int) string {
+	result := strconv.Itoa(value)
+	if value < 10 {
+		result = "0" + result
+	}
+	return result
+}
