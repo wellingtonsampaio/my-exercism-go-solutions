c80e99a34cd89bdc74b0b848be1c329263ca6e32
Wellington <wellingtonsampaio@gmail.com>
2017-04-22 23:37:42 -0300

    Solution for exercise Raindrops

--- raindrops/raindrops.go
+++ raindrops/raindrops.go
@@ -1,8 +1,38 @@
+// Package raindrops contains the implementation of the Exercism's go exercise 'raindrops'.
 package raindrops
 
+import (
+	"bytes"
+	"strconv"
+)
+
+// testVersion is the current version of the test
 const testVersion = 2
 
-func Convert(int) string
+// Convert convert a number to a string according to its factors
+func Convert(number int) string {
+	// for performance use bytes.Buffer to concatenate strings
+	var result bytes.Buffer
 
-// Don't forget the test program has a benchmark too.
-// How fast does your Convert convert?
+	// If the number has 3 as a factor, add 'Pling'
+	if number%3 == 0 {
+		result.WriteString("Pling")
+	}
+
+	// If the number has 5 as a factor, add 'Plang'
+	if number%5 == 0 {
+		result.WriteString("Plang")
+	}
+
+	// If the number has 7 as a factor, add 'Plong'
+	if number%7 == 0 {
+		result.WriteString("Plong")
+	}
+
+	// Otherwise add the digits
+	if result.Len() == 0 {
+		result.WriteString(strconv.Itoa(number))
+	}
+
+	return result.String()
+}
