f59ccea84f356fd1a0c24ae37095d6ae37bcdaad
Wellington <wellingtonsampaio@gmail.com>
2017-04-24 20:44:56 -0300

    Solution for exercise Acronym

--- acronym/acronym.go
+++ acronym/acronym.go
@@ -1,5 +1,26 @@
+// Package acronym contains the implementation of the Exercism's go exercise 'acronym'
 package acronym
 
+import (
+	"bytes"
+	"regexp"
+	"strings"
+)
+
+// testVersion is the current version of the test
 const testVersion = 2
 
-func Abbreviate(string) string
+// Abbreviate returns the acronym of the given input phrase
+func Abbreviate(input string) string {
+	var result bytes.Buffer
+
+	// split the input into tokens (maybe yperText could be handled differently)
+	fields := regexp.MustCompile("\\s|-|yperTex").Split(input, -1)
+	for _, field := range fields {
+		// and get the first char of each token
+		result.WriteString(string(field[0]))
+	}
+
+	// return converting the acronym to uppercase
+	return strings.ToUpper(result.String())
+}
