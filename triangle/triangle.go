d18b4fc3b05de404532ad6de828fb3c1accb09ec
Wellington <wellingtonsampaio@gmail.com>
2017-05-16 20:36:09 -0300

    Fetch exercise Triangle

---
+++ triangle/triangle.go
@@ -0,0 +1,16 @@
+package triangle
+
+const testVersion = 3
+
+func KindFromSides(a, b, c float64) Kind
+
+// Notice KindFromSides() returns this type. Pick a suitable data type.
+type Kind
+
+// Pick values for the following identifiers used by the test program.
+NaT // not a triangle
+Equ // equilateral
+Iso // isosceles
+Sca // scalene
+
+// Organize your code for readability.
