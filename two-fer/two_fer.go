081037d4cbfb933cbb55b622c07c04475ac67762
Wellington <wellingtonsampaio@gmail.com>
2018-01-04 21:13:51 +0100

    Solution for exercise Two Fer

--- two-fer/two_fer.go
+++ two-fer/two_fer.go
@@ -1,15 +1,16 @@
-// This is a "stub" file.  It's a little start on your solution.
-// It's not a complete solution though; you have to write some code.
-
-// Package twofer should have a package comment that summarizes what it's about.
-// https://golang.org/doc/effective_go.html#commentary
+// Package twofer contains the implementation of the Exercism's go exercise 'Two Fer'
 package twofer
 
-// ShareWith should have a comment documenting it.
-func ShareWith(string) string {
-	// Write some code here to pass the test suite.
-	// Then remove all the stock comments.
-	// They're here to help you get started but they only clutter a finished solution.
-	// If you leave them in, reviewers may protest!
-	return ""
+import "fmt"
+
+// baseOutput is the base phrase that gets replaces according to the input
+const baseOutput string = "One for %v, one for me."
+
+// ShareWith determines the output of the 2-fer based on the input
+func ShareWith(input string) string {
+	value := input
+	if len(input) == 0 {
+		value = "you"
+	}
+	return fmt.Sprintf(baseOutput, value)
 }
